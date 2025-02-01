e0c72fb5fb255dc145d9548fbb92fd12773dc590
Michelle Tolentino <misheruvynx@gmail.com>
2025-02-01 10:29:24 +0800

    added header

--- src/App.js
+++ src/App.js
@@ -3,21 +3,36 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <div className="header">
+        <h1>Monte Carlo Simulation Prototype</h1>
+        <div className='header-right-section'>
+          <div className='header-icon-image-right-section'>
+            <img src="/video-icon.png" className="video-icon-logo" alt="video-icon"/>
+            <p>Video Demo</p>
+          </div>
+          <div className='header-icon-image-right-section'>
+            <img src="/info-icon.png" className="info-icon-logo" alt="info-icon"/>
+            <p>Documentation</p>
+          </div>
+        </div>
+      </div>
+      <div className='body'>
+        <div className='instructions'>
+
+        </div>
+        <div className='monte-carlo-sim-container'>
+          <div className='hist-cat-container'>
+          
+          </div>
+          <div className='computations-container'>
+          
+          </div>
+          <div className='simulations-container'>
+          
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
